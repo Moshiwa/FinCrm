09e620d89ddd108135bd903cdbd7e896509d1b8b
“Manchin” <leha.manchin@mail.ru>
2023-02-02 13:42:04 +0300

    Установил vusjs и elplus

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 import './bootstrap';
+import {createApp} from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
+
+if (document.getElementById("vue-app")) {
+
+    const app = createApp({})
+        .use(ElementPlus)
+        .mixin({
+            components: {
+
+            }
+        });
+    //app.config.globalProperties.$dayjs = dayjs
+
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+        app.component(key, component)
+    }
+
+    app.mount('#vue-app')
+}
