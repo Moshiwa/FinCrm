e86a5469a03ee51179587327bd7f45520c36c477
“Manchin” <leha.manchin@mail.ru>
2023-03-16 15:38:47 +0300

    Избавился от taskOperation

--- app/Http/Controllers/Admin/TaskCrudController.php
+++ app/Http/Controllers/Admin/TaskCrudController.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 class TaskCrudController extends CrudController
 {
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use TaskOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 
     public function setup()
     {
@@ -35,6 +35,28 @@ class TaskCrudController extends CrudController
 
         CRUD::column('name');
         CRUD::column('start');
+    }
+
+    public function show($id)
+    {
+        $this->crud->hasAccessOrFail('show');
+
+        // get entry ID from Request (makes sure its the last ID for nested resources)
+        $id = $this->crud->getCurrentEntryId() ?? $id;
+
+        // get the info for that entry (include softDeleted items if the trait is used)
+        if ($this->crud->get('show.softDeletes') && in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($this->crud->model))) {
+            $this->data['entry'] = $this->crud->getModel()->withTrashed()->findOrFail($id);
+        } else {
+            $this->data['entry'] = $this->crud->getEntryWithLocale($id);
+        }
+
+        $this->data['crud'] = $this->crud;
+        $this->data['task'] = $this->data['entry'];
+        $this->data['title'] = $this->crud->getTitle() ?? trans('backpack::crud.preview').' '.$this->crud->entity_name;
+
+        // load the view from /resources/views/vendor/backpack/crud/ if it exists, otherwise load the one in the package
+        return view('crud::detail_task', $this->data);
     }
 
     private function hiddenExecutorFilter()
@@ -150,7 +172,7 @@ class TaskCrudController extends CrudController
             'executor_id' => null,
         ]);
 
-        return redirect('/admin/task/' . $task->id . '/detail');
+        return redirect('/admin/task/' . $task->id . '/show');
     }
 
     public function delete(Task $task, Request $request)
