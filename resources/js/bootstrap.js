7cd605d6529c2576d900888ecee9b0103acfb06f
“Manchin” <leha.manchin@gmail.com>
2023-03-24 17:20:28 +0300

    sockets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -34,13 +34,13 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: false,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
+    broadcaster:       'pusher',
+    key:               import.meta.env.VITE_PUSHER_APP_KEY,
+    wsHost:            window.location.hostname,
+    wsPort:            6009,
+    wssPort:           6009,
+    forceTLS:          false,
+    disableStats:      true,
     enabledTransports: ['ws', 'wss'],
 });
 // import Echo from 'laravel-echo';
