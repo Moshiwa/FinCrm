2f742c883149a9b7305ef26a0cdfac4961569f36
“Manchin” <leha.manchin@gmail.com>
2023-03-24 17:03:07 +0300

    sockets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,6 +18,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
 import Echo from "laravel-echo"
 
 /*window.Pusher = require('pusher-js');*/
@@ -27,13 +36,11 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: false,
     wsHost: window.location.hostname,
     wsPort: 6001,
     forceTLS: false,
     disableStats: true,
-    enabledTransports: ['ws', 'wss'],
-})
+});
 // import Echo from 'laravel-echo';
 
 // import Pusher from 'pusher-js';
