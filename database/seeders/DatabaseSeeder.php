eafc297c28617562d107483b8e364c91aab024e4
“Manchin” <leha.manchin@gmail.com>
2023-03-31 17:24:08 +0300

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,20 +13,8 @@ class DatabaseSeeder extends Seeder
         (new FieldTypesSeeder())->run();
         (new SpacesSeeder())->run();
         (new PermissionSeeder())->run();
-        (new PipelineSeeder())->run();
-        (new StagesSeeder())->run();
-        (new SettingsSeeder())->run();
         (new UsersSeeder())->run();
 
-        TaskStage::query()->firstOrcreate(['name' => 'Новая'], [
-            'name' => 'Новая'
-        ]);
-        TaskStage::query()->firstOrcreate(['name' => 'Завершена'], [
-            'name' => 'Завершена'
-        ]);
-
-
-        (new TestSeeder())->run();
         (new FieldsSeeder())->run();
 
 
--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -10,20 +10,20 @@ class UsersSeeder extends Seeder
 {
     public function run()
     {
-        $user = User::query()->firstOrCreate([ 'email' => 'admin@mail.ru' ], [
+        $user = User::query()->firstOrCreate(['email' => 'admin@mail.ru'], [
             'name' => 'admin',
             'email' => 'admin@mail.ru',
             'password' => bcrypt('itpolice')
         ]);
         $user->roles()->sync(1);
 
-        User::query()->firstOrCreate([ 'email' => 'test@mail.ru' ], [
+        User::query()->firstOrCreate(['email' => 'test@mail.ru'], [
             'name' => 'Олег Петрович',
             'email' => 'test@mail.ru',
             'password' => bcrypt('test')
         ]);
 
-        User::query()->firstOrCreate([ 'email' => 'test1@mail.ru' ], [
+        User::query()->firstOrCreate(['email' => 'test1@mail.ru'], [
             'name' => 'Сифон Паршев',
             'email' => 'test1@mail.ru',
             'password' => bcrypt('test1')
