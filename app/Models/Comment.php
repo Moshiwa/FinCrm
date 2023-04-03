486af60e2f8e43a46580b7fc201fce5e9e2e7246
“Manchin” <leha.manchin@gmail.com>
2023-04-03 13:05:41 +0300

    fix manager

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\SpaceableTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,6 +10,8 @@ use Illuminate\Support\Carbon;
 
 class Comment extends Model
 {
+    use SpaceableTrait;
+
     protected $fillable = [
         'deal_id',
         'type',
