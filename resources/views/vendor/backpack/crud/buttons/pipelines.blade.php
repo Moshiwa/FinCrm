58d3fca5f195b5d2bf2480c8a6e5a2259547d9a1
Manchin <leha.manchin@gmail.com>
2023-06-23 13:58:06 +0300

    поправил стили кнопок с воронками

--- resources/views/vendor/backpack/crud/buttons/pipelines.blade.php
+++ resources/views/vendor/backpack/crud/buttons/pipelines.blade.php
@@ -4,9 +4,9 @@
     $default = $pipelines->first()->id;
     $pipeline_id = $crud->getRequest()->get('pipeline');
 ?>
-<div style="display: flex; gap: 10px; margin: 10px 0 10px 0;">
+<div style="display: flex; flex-wrap: wrap; gap: 10px; margin: 10px 0 10px 0;">
     @foreach($pipelines as $pipeline)
-        <a href="/admin/deal?pipeline={{ $pipeline->id }}" class="btn btn-outline-primary {{$pipeline_id == $pipeline->id ? 'active' : ''}}">
+        <a href="/admin/deal?pipeline={{ $pipeline->id }}" style="max-width: 32%; min-width: 32%" class="btn btn-outline-primary {{$pipeline_id == $pipeline->id ? 'active' : ''}}">
             <span class="ladda-label"><i class="la la-eye"></i> {{$pipeline->name}} </span>
         </a>
     @endforeach
