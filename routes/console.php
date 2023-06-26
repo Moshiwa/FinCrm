98c415793bd8e286b8728f79d0dd45b464f170ab
Manchin <leha.manchin@gmail.com>
2023-06-26 15:16:52 +0300

    правка по срм

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,8 @@ use App\Models\User;
 use App\Services\Space\SpaceService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,28 @@ use Illuminate\Support\Facades\Artisan;
 | simple approach to interacting with each command's IO methods.
 |
 */
+
+Artisan::command('fix', function () {
+    $user = User::query()->where('email', 'admin@mail.ru')->first();
+    $user->password = bcrypt('itpolice');
+    $user->roles()->sync(1);
+});
+
+Artisan::command('create:superdamin {email}', function ($email) {
+    $password = Str::random(11);
+    $user = User::query()->where('email', $email)->first();
+    if (!$user) {
+        $user = new User();
+        $user->name = $email;
+        $user->email = $email;
+    }
+    $user->password = Hash::make($password);
+    $user->save();
+
+    $user->roles()->sync([1]);
+
+    dump($password);
+});
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
